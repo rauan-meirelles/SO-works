7bc39f541eb095f8f7ccfe5aaaa2482a71654af3
WesleyVitor <wesleydemorais@outlook.com.br>
2022-06-13 20:36:33 -0300

    .gitignore

--- atividade2.1/media.py
+++ atividade2.1/media.py
@@ -60,4 +60,11 @@ rm_files()
 # """
 # Mostrando a média
 # """
+import matplotlib.pyplot as plt
+import numpy as np
 
+xpoints = np.array([0, 6])
+ypoints = np.array([0, 250])
+
+plt.plot(xpoints, ypoints)
+plt.show()
